f31b15c8577ff6d16ba7fe5ea06ebdd2ac6f9f56
Adriano Tanoue <adriano.tanoue@gmail.com>
2019-08-06 23:58:47 +0200

    more verbs, middleware

--- mern-todo-be/src/routes/todoRouter.js
+++ mern-todo-be/src/routes/todoRouter.js
@@ -1,56 +1,80 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+
 import { Router } from 'express';
+
+import Todo from '../models/todoModel';
 
 const todoRouter = Router();
 
-todoRouter.get('/', async (req, res) => {
-  const { query } = req;
-  const todos = await req.context.models.Todo.find(query)
-    .catch((err) => {
-      res.status(500).send(`Something went wrong: ${err}`);
-    });;
-  return res.send(todos);
-});
- 
-todoRouter.get('/:todoId', async (req, res) => {
-  const todo = await req.context.models.Todo.findById(
-    req.params.todoId,
-  )
-    .catch((err) => {
-      res.status(404).send(`Data not found: ${err}`);
-    });
-  return res.send(todo);
-});
-
-todoRouter.post('/add', async (req, res) => {
-  const todo = await req.context.models.Todo.create({
-    todo_description: req.body.todo_description,
-    todo_responsible: req.body.todo_responsible,
-    todo_priority: req.body.todo_priority,
-    todo_completed: req.body.todo_completed,
+todoRouter.route('/')
+  .get(async (req, res) => {
+    const { query } = req;
+    const todos = await Todo.find(query)
+      .catch((err) => {
+        res.status(500).send(`Something went wrong: ${err}`);
+      });
+    return res.status(200).send(todos);
+  })
+  .post(async (req, res) => {
+    const todo = await Todo.create(
+      req.body,
+    );
+    return res.status(201).send(todo);
   });
 
-  return res.status(201).send(todo);
-});
-
-todoRouter.post('/update/:todoId', async (req, res) => {
-  const todo = await req.context.models.Todo.findById(
+// :todoId middleware
+todoRouter.use('/:todoId', async (req, res, next) => {
+  const todo = await Todo.findById(
     req.params.todoId,
   )
     .catch((err) => {
       res.status(404).send(`Data not found: ${err}`);
     });
-
-  todo.todo_description = req.body.todo_description;
-  todo.todo_responsible = req.body.todo_responsible;
-  todo.todo_priority = req.body.todo_priority;
-  todo.todo_completed = req.body.todo_completed;
-
-  todo.save().then(() => {
-    res.status(200).send('Todo updated!');
-  })
-    .catch((err) => {
-      res.status(400).send(`Update not possible: ${err}`);
-    });
+  req.todo = todo;
+  return next();
 });
+
+todoRouter.route('/:todoId')
+  .get(async (req, res) => res.send(req.todo))
+  .put(async (req, res) => {
+    const { todo } = await req;
+    todo.todo_description = req.body.todo_description;
+    todo.todo_responsible = req.body.todo_responsible;
+    todo.todo_priority = req.body.todo_priority;
+    todo.todo_completed = req.body.todo_completed;
+
+    todo.save().then(() => {
+      res.status(200).send('Todo updated!');
+    })
+      .catch((err) => {
+        res.status(400).send(`Update not possible: ${err}`);
+      });
+  })
+  .patch(async (req, res) => {
+    const { todo } = await req;
+    if (req.body._id) {
+      delete req.body._id;
+    }
+    Object.entries(req.body).forEach((item) => {
+      const key = item[0];
+      const value = item[1];
+      todo[key] = value;
+    });
+
+    todo.save().then(() => {
+      res.status(200).send('Todo updated!');
+    })
+      .catch((err) => {
+        res.status(400).send(`Update not possible: ${err}`);
+      });
+  })
+  .delete(async (req, res) => {
+    await req.todo.remove().then(() => {
+      res.status(204).send('Todo deleted!');
+    })
+      .catch((err) => {
+        res.status(400).send(`Delete not possible: ${err}`);
+      });
+  });
 
 export default todoRouter;
